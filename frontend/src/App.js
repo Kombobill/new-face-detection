9e848ee545456da53315b98bfa04370f067bc0c7
Kombobill <billmadru25@gmail.com>
2024-06-20 00:13:56 +0300

    intergrating web cam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Webcam from 'react-webcam';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,9 @@ class App extends Component {
       input: '',
       imageUrl: '',
       boxes: [],
+      cameraActive: false,
     };
+    this.webcamRef = React.createRef();
   }
 
   calculateFaceLocation = (data) => {
@@ -40,8 +43,20 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  capture = () => {
+    const imageSrc = this.webcamRef.current.getScreenshot();
+    this.setState({ imageUrl: imageSrc });
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/imageurl`, { input: imageSrc })
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response.data)))
+      .catch(err => console.log(err));
+  }
+
+  toggleCamera = () => {
+    this.setState((prevState) => ({ cameraActive: !prevState.cameraActive }));
+  }
+
   render() {
-    const { imageUrl, boxes } = this.state;
+    const { imageUrl, boxes, cameraActive } = this.state;
     return (
       <div className="App">
         <p>{'This Magic Brain will detect faces in your pictures. Give it a try.'}</p>
@@ -49,10 +64,37 @@ class App extends Component {
           <input type="text" onChange={this.onInputChange} />
           <button onClick={this.onButtonSubmit}>Detect</button>
         </div>
+        <div>
+          <button onClick={this.toggleCamera}>
+            {cameraActive ? 'Stop Camera' : 'Start Camera'}
+          </button>
+          {cameraActive && (
+            <div>
+              <Webcam
+                audio={false}
+                ref={this.webcamRef}
+                screenshotFormat="image/jpeg"
+                width={500}
+              />
+              <button onClick={this.capture}>Capture</button>
+            </div>
+          )}
+        </div>
         <div className="image-container">
-          <img id="inputImage" alt="" src={imageUrl} width="500px" heigh="auto"/>
+          <img id="inputImage" alt="" src={imageUrl} width="500px" heigh="auto" />
           {boxes.map((box, i) => {
-            return <div key={i} className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            return (
+              <div
+                key={i}
+                className="bounding-box"
+                style={{
+                  top: box.topRow,
+                  right: box.rightCol,
+                  bottom: box.bottomRow,
+                  left: box.leftCol,
+                }}
+              ></div>
+            );
           })}
         </div>
       </div>
